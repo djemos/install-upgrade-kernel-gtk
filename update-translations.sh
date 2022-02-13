517569ff3a3a512f796726e932682610a315512c
djemos <dijemos@gmail.com>
2022-02-13 18:02:55 +0200

    install-upgrade-kerne-gtk

---
+++ update-translations.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+intltool-extract --type="gettext/ini" src/install-upgrade-kernel-gtk.desktop.in
+intltool-extract --type="gettext/ini" src/install-upgrade-kernel-gtk-kde.desktop.in
+
+xgettext --from-code=utf-8 \
+	-L Glade \
+	-o po/install-upgrade-kernel-gtk.pot \
+	src/install-upgrade-kernel-gtk.ui
+
+xgettext --from-code=utf-8 \
+	-j \
+	-L Python \
+	-o po/install-upgrade-kernel-gtk.pot \
+	src/install-upgrade-kernel-gtk.c
+xgettext --from-code=utf-8 -j -L C -kN_ -o po/install-upgrade-kernel-gtk.pot src/install-upgrade-kernel-gtk.desktop.in.h
+xgettext --from-code=utf-8 -j -L C -kN_ -o po/install-upgrade-kernel-gtk.pot src/install-upgrade-kernel-gtk-kde.desktop.in.h
+
+rm src/install-upgrade-kernel-gtk.desktop.in.h src/install-upgrade-kernel-gtk-kde.desktop.in.h
+
+cd po
+for i in `ls *.po`; do
+	msgmerge -U $i install-upgrade-kernel-gtk.pot
+done
+rm -f ./*~
+
+
